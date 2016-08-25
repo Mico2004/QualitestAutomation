8271f20a97268a0313c01acfe30fb8f97a44413a
Mickael <mickael.elimelech@qualitest.co.il>
2016-08-25 17:48:26 +0300

    Fixed unsynced admindashboard helper

--- WebDriverTest/src/test/java/com/automation/main/AdminDashboardPage.java
+++ WebDriverTest/src/test/java/com/automation/main/AdminDashboardPage.java
@@ -1,8 +1,11 @@
 package com.automation.main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -25,6 +28,9 @@ import atu.testng.selenium.reports.CaptureScreen.ScreenshotOf;
 
 @Listeners({ ATUReportsListener.class, ConfigurationListener.class, MethodListener.class })
 public class AdminDashboardPage extends Page {
+	
+	WebElement targetLink;
+	
 	// Set Property for ATU Reporter Configuration
 	{
 		System.setProperty("atu.reporter.config", "src/test/resources/atu.properties");
@@ -34,6 +40,9 @@ public class AdminDashboardPage extends Page {
 	public AdminDashboardPage(WebDriver browser) {
 		super(browser);
 		// TODO Auto-generated constructor stub
+		
+		
+		
 	}
 
 	@FindBy(css = "#CoursesBox>ul>li>a")
@@ -43,34 +52,95 @@ public class AdminDashboardPage extends Page {
 	public
 	List<WebElement> users_submenu;
 	@FindBy(css="#ServiceSettingsBox>ul>li>a") List<WebElement> services_setting_submenu;
-	@FindBy (xpath="//*[@id='CoursesBox']/ul/li[4]/a") WebElement manageAdHockCourses;
+	@FindBy (xpath="//*[@id='CoursesBox']/ul/li/a[text() = 'Manage Ad-hoc Courses / Enrollments (Course Builder)']") 
+	WebElement manageAdHockCourses;
+	@FindBy (xpath="//*[@id='CoursesBox']/ul/li/a[text() = 'View Course List']") 
+	WebElement viewCourseList;
 
+	@FindBy (xpath="//*[@id='CoursesBox']/ul/li/a[text() = 'Manage Course Settings']") 
+	WebElement manageCourseSettings;
+
+	@FindBy (xpath="//*[@id='CoursesBox']/ul/li/a[text() = 'View Archive']") 
+	WebElement viewArchive;
+
+	@FindBy (id="StatusBox")
+	WebElement StatusBox;
+	@FindBy (id="RecordersRecordingsBox")
+	WebElement RecordersRecordingsBox;
+	@FindBy (id="UsersBox")
+	WebElement UsersBox;
+	@FindBy (id="ServiceSettingsBox")
+	WebElement ServiceSettingsBox;
+	@FindBy (id="EventsAlertsBox")
+	WebElement EventsAlertsBox;
+	@FindBy (id="CoursesBox")
+	WebElement CoursesBox;
+	@FindBy (id="IntegrationBox")
+	WebElement IntegrationBox;
+	List <WebElement> dashboardSections;
+	String linkText="";
 	
 	// This function get String with the name of target submenu of Courses and clicks on it
 	public void clickOnTargetSubmenuCourses(String target) {
 //		wait.until(ExpectedConditions.visibilityOf(courses_submenu.get(0)));
-		for (int i = 0; i < courses_submenu.size(); i++) {
-			if (courses_submenu.get(i).getText().equals(target)) {
-				try {
-					Thread.sleep(1500);
-					waitForVisibility(courses_submenu.get(i));
-					courses_submenu.get(i).click();					
-					System.out.println("Click on target submenu of Courses: " + target);
-					ATUReports.add("Click on target submenu of Courses.", "Clicked on target submenu.", "Clicked on target submenu.", LogAs.PASSED, null);
+
+
+
+
+
+
+		try {
+		System.out.println("clickOnTargetSubmenuCourses1");
+		waitForVisibility(CoursesBox);
+		System.out.println("clickOnTargetSubmenuCourses2");
+	//	wait.until(ExpectedConditions.visibilityOfAllElements(dashboardSections));
+		System.out.println("clickOnTargetSubmenuCourses3");
+		Thread.sleep(4000);
+		System.out.println("clickOnTargetSubmenuCourses4");
+		
+		switch(target){
+		case "View Course List": 
+			targetLink=viewCourseList;
+			break;
+		case "View Archive": 
+			targetLink=viewArchive;
+			break;
+		case "Manage Course Settings": 
+			targetLink=manageCourseSettings;
+			break;
+		case "Manage Ad-hoc Courses / Enrollments (Course Builder)": 
+			targetLink=manageAdHockCourses;
+			break;		
+			default: targetLink=manageAdHockCourses;
+		}
+		linkText=targetLink.getText();
+		System.out.println("clickOnTargetSubmenuCourses5");
+				
+					waitForVisibility(targetLink);
+					System.out.println("clickOnTargetSubmenuCourses6");
+					targetLink.click();					
+					System.out.println("clickOnTargetSubmenuCourses7");
+					System.out.println("Click on target submenu of Courses: "+linkText  );
+					ATUReports.add("Click on target submenu of Courses:"+linkText, "Clicked on target submenu.", "Clicked on target submenu.", LogAs.PASSED, null);
+					Assert.assertTrue(true);
 					return;
 				} catch (Exception msg) {
-					System.out.println("Not click on target submenu of Courses: " + target);
+					System.out.println("Not click on target submenu of Courses: " + targetLink.getText().toString());
 					System.out.println("ERROR msg: " + msg.getMessage());
-					ATUReports.add("Click on target submenu of Courses.", "Clicked on target submenu.", "Not click on target submenu.", LogAs.FAILED, null);
+					ATUReports.add("Click on target submenu of Courses:"+linkText, "Clicked on target submenu.", "Not click on target submenu.", LogAs.FAILED, null);
+					Assert.assertTrue(false);
 					return;
-				}
 
-			}
+				
+
+
+			
 		}
 		
-		System.out.println("Not found the target submenu to click on: " + target);
-		ATUReports.add("Click on target submenu of Courses.", "Clicked on target submenu.", "Target submenu not found.", LogAs.FAILED, null);
-		Assert.assertTrue(false);
+
+
+
+		
 	}
 
 	
