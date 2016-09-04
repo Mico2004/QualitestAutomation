1736073fe8df53a22176d99dad1a86a2cc9542d7
Mickael <mickael.elimelech@qualitest.co.il>
2016-09-04 14:45:57 +0300

    clickOnTargetSubmenuUsers() Changed implementation for clicking one of the 'Users' links

--- WebDriverTest/src/test/java/com/automation/main/AdminDashboardPage.java
+++ WebDriverTest/src/test/java/com/automation/main/AdminDashboardPage.java
@@ -62,7 +62,19 @@ public class AdminDashboardPage extends Page {
 
 	@FindBy (xpath="//*[@id='CoursesBox']/ul/li/a[text() = 'View Archive']") 
 	WebElement viewArchive;
-
+	@FindBy (xpath="//*[@id='UsersBox']/ul/li/a[text() = 'Manage Admin Roles']") 
+	WebElement manageAdminRoles;
+	@FindBy (xpath="//*[@id='UsersBox']/ul/li/a[text() = 'Manage Admin Users']") 
+	WebElement manageAdminUsers;
+	@FindBy (xpath="//*[@id='UsersBox']/ul/li/a[text() = 'Manage Excel Users']") 
+	WebElement manageExcelUsers;
+	@FindBy (xpath="//*[@id='UsersBox']/ul/li/a[text() = 'Manage Self Registration Users']") 
+	WebElement manageSelfRegistration;
+	@FindBy (xpath="//*[@id='UsersBox']/ul/li/a[text() = 'Manage Ad-hoc Users (User Builder)']") 
+	WebElement manageAdHocUsers;
+	@FindBy (xpath="//*[@id='UsersBox']/ul/li/a[text() = 'Impersonate User']") 
+	WebElement ImpersonateUser;
+	
 	@FindBy (id="StatusBox")
 	WebElement StatusBox;
 	@FindBy (id="RecordersRecordingsBox")
@@ -81,7 +93,8 @@ public class AdminDashboardPage extends Page {
 	String linkText="";
 	
 	// This function get String with the name of target submenu of Courses and clicks on it
-	public void clickOnTargetSubmenuCourses(String target) {
+	public void clickOnTargetSubmenuCourses(String target) 
+	{
 //		wait.until(ExpectedConditions.visibilityOf(courses_submenu.get(0)));
 
 
@@ -143,54 +156,72 @@ public class AdminDashboardPage extends Page {
 
 	
 	// This function get String with the name of target submenu of Users and clicks on it
-	public void clickOnTargetSubmenuUsers(String target) throws InterruptedException {
+	public void clickOnTargetSubmenuUsers(String target) throws InterruptedException 
 		
-//		wait.until(ExpectedConditions.visibilityOfAllElements(users_submenu));
-//		wait.until(ExpectedConditions.visibilityOf(users_submenu.get(0)));
-//		wait.until(ExpectedConditions.visibilityOf(users_submenu.get(1)));
-		
-		for(int i=0; i<10; i++) {
-			try {
-				if(users_submenu.size()>0) {
-					break;
-				}
-				Thread.sleep(1000);
-			} catch (Exception msg) {
-				Thread.sleep(1000);
-			}
-		}
+	{
+//		wait.until(ExpectedConditions.visibilityOf(courses_submenu.get(0)));
 
-		for (int i = 0; i < users_submenu.size(); i++) {
-			
-			if (users_submenu.get(i).getText().equals(target)) {
-				try {
-					users_submenu.get(i).click();
-					System.out.println("Click on target submenu of Users: " + target);
-					ATUReports.add("Click on target submenu of Users.", "Clicked on target submenu.", "Clicked on target submenu.", LogAs.PASSED, null);
+
+
+
+		try {
+		System.out.println("clickOnTargetSubmenuUsers1");
+		waitForVisibility(CoursesBox);
+		System.out.println("clickOnTargetSubmenuUsers2");
+	//	wait.until(ExpectedConditions.visibilityOfAllElements(dashboardSections));
+		System.out.println("clickOnTargetSubmenuUsers3");
+		Thread.sleep(2000);
+		System.out.println("clickOnTargetSubmenuUsers4");
+		
+		switch(target){
+		case "Impersonate User": 
+			targetLink=ImpersonateUser;
+			break;
+		case "Manage Ad-hoc Users (User Builder)": 
+			targetLink=manageAdHocUsers;
+			break;
+		case "Manage Self Registration Users": 
+			targetLink=manageSelfRegistration;
+			break;
+		case "Manage Excel Users": 
+			targetLink=manageExcelUsers;
+			break;
+		case "Manage Admin Users": 
+			targetLink=manageAdminUsers;
+			break;		
+		case "Manage Admin Roles": 
+			targetLink=manageAdminRoles;
+			break;
+			default: targetLink=manageAdHocUsers;
+		}
+		linkText=targetLink.getText();
+		System.out.println("clickOnTargetSubmenuUsers5");
+				
+					waitForVisibility(targetLink);
+					System.out.println("clickOnTargetSubmenuUsers6");
+					targetLink.click();					
+					System.out.println("clickOnTargetSubmenuUsers7");
+					System.out.println("clickOnTargetSubmenuUsers: "+linkText  );
+					ATUReports.add("Click on target submenu of Users:"+linkText, "Clicked on target submenu.", "Clicked on target submenu.", LogAs.PASSED, null);
+					Assert.assertTrue(true);
 					return;
 				} catch (Exception msg) {
-					System.out.println("Not click on target submenu of Users menu: " + target);
-					System.out.println("ERROR msg: " + msg);
-					ATUReports.add("Click on target submenu of Users.", "Clicked on target submenu.", "Not clicked on target submenu.", LogAs.FAILED, null);
+					System.out.println("Not click on target submenu of Users: " + targetLink.getText().toString());
+					System.out.println("ERROR msg: " + msg.getMessage());
+					ATUReports.add("Click on target submenu of Users:"+linkText, "Clicked on target submenu.", "Not click on target submenu.", LogAs.FAILED, null);
+					Assert.assertTrue(false);
 					return;
-				}
 
-			}
+				
+
+
+			
 		}
 		
-		
-		
-		System.out.println("Not found the target submenu to click on: " + target);
-		ATUReports.add("Click on target submenu of Users.", "Clicked on target submenu.", "Target submenu not found.", LogAs.FAILED, null);
-		Assert.assertTrue(false);
-	}
 
-	// This function go to admin dashboard by using its url
-	public void goToAdminDashboard() {
-		String login_url = driver.getCurrentUrl();
-		String university_name = login_url.split("/")[2].substring(0, login_url.split("/")[2].length() - 12);
-		String course_page_url = "https://" + university_name + ".tegrity.com/#/dashboard";
-		driver.navigate().to(course_page_url);
+
+
+		
 	}
 
 	
