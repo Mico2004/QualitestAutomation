1b610a8bb56014a13560c30a5d410e1a17432211
Mickael <mickael.elimelech@qualitest.co.il>
2016-09-13 16:32:00 +0300

    Added logs

--- WebDriverTest/src/test/java/com/automation/main/LoginHelperPage.java
+++ WebDriverTest/src/test/java/com/automation/main/LoginHelperPage.java
@@ -129,7 +129,7 @@ public class LoginHelperPage extends Page {
 			
 			
 		}
-
+		System.out.println("loginCourses7");
 		Thread.sleep(1000);
 		if (driver.getCurrentUrl().contains("eula")) {
 			try {
@@ -139,7 +139,7 @@ public class LoginHelperPage extends Page {
 				System.out.println("No EULA button.");
 			}
 		}
-
+		System.out.println("loginCourses8");
 		for (int second = 0;; second++) {
 			if (second >= 60)
 				Assert.fail("timeout");
@@ -160,6 +160,7 @@ public class LoginHelperPage extends Page {
 
 			Thread.sleep(1000);
 		}
+		System.out.println("loginCourses8");
 		Thread.sleep(3000);
 		Assert.assertEquals(driver.getTitle(), "Tegrity - Courses");
 	}
