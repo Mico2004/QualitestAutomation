4bf86d4ace6550ae74065914f7b3796af65d3786
Mickael <mickael.elimelech@qualitest.co.il>
2016-09-15 15:22:34 +0300

    1

--- WebDriverTest/src/test/java/com/automation/main/LoginHelperPage.java
+++ WebDriverTest/src/test/java/com/automation/main/LoginHelperPage.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.support.FindBy;
-
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
