1ebac4ec432442cd7b4ca0d5fb9a1a9427becdb0
mickaele <mickaele@QA-Mickael-Lap.Tegrity.local>
2016-09-22 18:38:03 +0300

    fluentWaitInvisibility() Change failure to warning
    Added function waitForContentOfTabToLoad()

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/Page.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/Page.java
@@ -16,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
