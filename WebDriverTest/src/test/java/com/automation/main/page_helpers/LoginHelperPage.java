c083ef8e216a38cc0497addce451a2a9ef1faa69
Moshik Kalash <moshik.kalash@qualitest.co.il>
2016-10-13 17:02:15 +0300

    1

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/LoginHelperPage.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/LoginHelperPage.java
@@ -57,13 +57,8 @@ public class LoginHelperPage extends Page {
 		setPageTitle("Tegrity Lecture Capture");	
 
 		//setPageUrl("https://awsserverautomation-qa-5.tegrity.com/#/login");
-
-		//setPageUrl("https://awsserverautomation-perf-5.tegrity.com/#/login");	
-		setPageUrl(DriverSelector.setDriverUniversity(System.getProperty("University")));
-
-
-	
-
+		setPageUrl("https://awsserverautomation-perf-5.tegrity.com/#/login");	
+		//setPageUrl(DriverSelector.setDriverUniversity(System.getProperty("University")));
 
 	}
 
