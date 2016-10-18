5fe124340e0611b53195511e9e86d95578ae2970
Moshik Kalash <moshik.kalash@qualitest.co.il>
2016-10-18 08:59:24 +0300

    fixing the test 15654

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/CopyMenu.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/CopyMenu.java
@@ -151,8 +151,8 @@ public class CopyMenu extends Page {
 		String selected_course = null;
 		Thread.sleep(1500);
 		wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOf(first_course_on_the_list));
-		
+		wait.until(ExpectedConditions.textToBePresentInElement(first_course_on_the_list, target_course_name));
+	
 		for (int i = 0; i < course_list.size(); i++) {
 			selected_course = course_list.get(i).getText();
 			if (selected_course.equals(target_course_name)) {
