9b703851d43cebe75a70536efcb634faf45b561a
Moshik Kalash <moshik.kalash@qualitest.co.il>
2016-10-27 17:31:10 +0300

    do noting

--- WebDriverTest/src/test/java/com/automation/main/utilities/DriverSelector.java
+++ WebDriverTest/src/test/java/com/automation/main/utilities/DriverSelector.java
@@ -56,15 +56,16 @@ public class DriverSelector {
 		case Firefox:
 			System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");	
 			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			capabilities.setCapability("marionette", true);
-//			FirefoxProfile prfl = new FirefoxProfile();
-//		    prfl.setPreference("browser.startup.homepage", "about:blank");
-//		    prfl.setPreference("browser.startup.homepage_override.mstone", "ignore");
-//		    prfl.setPreference("startup.homepage_welcome_url", "about:blank");
-//		    prfl.setPreference("startup.homepage_welcome_url.additional", "about:blank");
-//		    capabilities.setCapability(FirefoxDriver.PROFILE, prfl);
+			//capabilities.setCapability("marionette", true);
+			FirefoxProfile prfl = new FirefoxProfile();
+		    prfl.setPreference("browser.startup.homepage", "about:blank");
+		    prfl.setPreference("browser.startup.homepage_override.mstone", "ignore");
+		    prfl.setPreference("startup.homepage_welcome_url", "about:blank");
+		    prfl.setPreference("startup.homepage_welcome_url.additional", "about:blank");    
+		    capabilities.setCapability(FirefoxDriver.PROFILE, prfl);
 			driver = new FirefoxDriver(capabilities);	
 			//driver = new FirefoxDriver();
+			//driver = new MarionetteDriver(capabilities);
 			break;
 		case Chrome:
 			System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
