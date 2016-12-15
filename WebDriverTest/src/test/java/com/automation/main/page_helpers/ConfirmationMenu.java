0e83c31925bff83eb5b0c40c42ff3e9985a57381
Moshik Kalash <moshik.kalash@qualitest.co.il>
2016-12-15 09:44:43 +0200

    change to js

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/ConfirmationMenu.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/ConfirmationMenu.java
@@ -69,7 +69,7 @@ public class ConfirmationMenu extends Page {
 				clickOkInAlertIfPresent();
 			}
 			waitForVisibility(ok_button);		
-			ok_button.click();
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();",ok_button);
 			ATUReports.add("Clicked on OK button.", LogAs.PASSED, null);
 			Assert.assertTrue(true);
 		} catch (Exception e) {
