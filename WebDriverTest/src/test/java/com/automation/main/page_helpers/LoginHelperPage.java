9d6b4899677be2b9300ccf2e904ef7b94bc2d896
MoshikK <MoshikK@QA-Lab12-W8.Tegrity.local>
2017-02-01 17:05:54 +0200

    sorry shahar

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/LoginHelperPage.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/LoginHelperPage.java
@@ -52,10 +52,10 @@ public class LoginHelperPage extends Page {
 		super(driver);
 
 		setPageTitle("Tegrity Lecture Capture");	
-		setPageUrl("https://awsserverautomation-qa-5.tegrity.com");	
+		//setPageUrl("https://awsserverautomation-qa-5.tegrity.com");	
 		//setPageUrl(DriverSelector.setDriverUniversity(System.getProperty("University")));
 		//setPageUrl("https://awsserverautomation3-qabr.tegrity.com");
-		///setPageUrl("https://awsserverautomation-perf-5.tegrity.com");	
+		setPageUrl("https://awsserverautomation-perf-5.tegrity.com");	
 		//setPageUrl(DriverSelector.setDriverUniversity(System.getProperty("University")));
 
 
