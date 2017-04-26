02e9de94c5d0ab4e8fb97fee07265ef837c25f8c
Ilan <ilan.adisa@qualitest.co.il>
2017-04-26 12:24:53 +0300

    The drivers should be located under "c:/selenium-drivers"

--- WebDriverTest/src/test/java/com/automation/main/utilities/DriverSelector.java
+++ WebDriverTest/src/test/java/com/automation/main/utilities/DriverSelector.java
@@ -23,113 +23,114 @@ import atu.testng.reports.listeners.MethodListener;
 import atu.testng.reports.logging.LogAs;
 import junitx.util.PropertyManager;
 
-@Listeners({ ATUReportsListener.class, ConfigurationListener.class, MethodListener.class })
+@Listeners({ATUReportsListener.class, ConfigurationListener.class, MethodListener.class})
 public class DriverSelector {
 
-	// Set Property for ATU Reporter Configuration
-	{
-		System.setProperty("atu.reporter.config", "src/test/resources/atu.properties");
+    private static String rootPath = "c:/selenium-drivers";
 
-	}
+    // Set Property for ATU Reporter Configuration
+    {
+        System.setProperty("atu.reporter.config", "src/test/resources/atu.properties");
 
-	public enum BrowserType {
-		Firefox("firefox"), IE("ie"), Chrome("chrome"), Safari("safari");
+    }
 
-		private String val;
+    public enum BrowserType {
+        Firefox("firefox"), IE("ie"), Chrome("chrome"), Safari("safari");
 
-		BrowserType(String value) {
-			this.val = value;
-		}
+        private String val;
 
-		public String getBrowserName() {
-			return val;
-		}
+        BrowserType(String value) {
+            this.val = value;
+        }
 
-	}
+        public String getBrowserName() {
+            return val;
+        }
 
-	@SuppressWarnings({ "deprecation", "null" })
-	public static WebDriver getDriver(BrowserType type) {
-		WebDriver driver = null;
-		DesiredCapabilities capability = null;
-		LoggingPreferences logPrefs = new LoggingPreferences();
-		logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-		switch (type) {
-		case Firefox:
-			System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");	
-			capability = DesiredCapabilities.firefox();		
-			capability.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);		
-			capability.setCapability("marionette", true);		
-			driver = new FirefoxDriver(capability);	
-			//driver = new MarionetteDriver();	
-			break;
-		case Chrome:
-			System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-		    capability = DesiredCapabilities.chrome();
-		    capability.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);		
-			driver = new ChromeDriver(capability);
-			driver.manage().window().maximize();
-			// capability=DesiredCapabilities.chrome();
-			// capability.setPlatform(Platform.WIN8_1);
-			break;
-		case IE:
-			System.setProperty("webdriver.ie.driver", "src/test/resources/IEDriverServer.exe");
-			capability = DesiredCapabilities.internetExplorer();
-			capability.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);	
-			capability.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);		
-			//capability.setCapability("nativeEvents",false);			
-			capability.setCapability("ignoreZoomSetting", true);
-			//capability.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
-			driver = new InternetExplorerDriver(capability);
-			driver.manage().window().maximize();
-			
-			break;
-		default:
-			System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
-			// MarionetteDriverManager.getInstance().setup();
-			// driver = new MarionetteDriver();
-			// System.setProperty("webdriver.gecko.driver",
-			// "src/test/resources");
-			driver = new MarionetteDriver();
-			//driver = new FirefoxDriver();		
-			//String marionetteDriverLocation = currentDir + "/tools/marionette/wires.exe";
-			//System.setProperty("webdriver.gecko.driver", marionetteDriverLocation);
-		     //driver = new MarionetteDriver();	
-			// DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			// capabilities.setCapability("marionette", true);
-			// driver = new FirefoxDriver(capabilities);
-			break;
-		}
-		return driver;
-	}
+    }
 
-	public static String setDriverUniversity(String university) throws IOException {
-			
-		return "https://"+university+".tegrity.com";
-	}
+    @SuppressWarnings({"deprecation", "null"})
+    public static WebDriver getDriver(BrowserType type) {
+        WebDriver driver = null;
+        DesiredCapabilities capability = null;
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+        switch (type) {
+            case Firefox:
+                System.setProperty("webdriver.gecko.driver", rootPath + "/geckodriver.exe");
+                capability = DesiredCapabilities.firefox();
+                capability.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+                capability.setCapability("marionette", true);
+                driver = new FirefoxDriver(capability);
+                //driver = new MarionetteDriver();
+                break;
+            case Chrome:
+                System.setProperty("webdriver.chrome.driver", rootPath + "/chromedriver.exe");
+                capability = DesiredCapabilities.chrome();
+                capability.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+                driver = new ChromeDriver(capability);
+                driver.manage().window().maximize();
+                // capability=DesiredCapabilities.chrome();
+                // capability.setPlatform(Platform.WIN8_1);
+                break;
+            case IE:
+                System.setProperty("webdriver.ie.driver", rootPath + "/IEDriverServer.exe");
+                capability = DesiredCapabilities.internetExplorer();
+                capability.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);
+                capability.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+                //capability.setCapability("nativeEvents",false);
+                capability.setCapability("ignoreZoomSetting", true);
+                //capability.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
+                driver = new InternetExplorerDriver(capability);
+                driver.manage().window().maximize();
 
-	public static BrowserType getBrowserTypeByProperty() {
-		BrowserType type = null;
+                break;
+            default:
+                System.setProperty("webdriver.gecko.driver", rootPath + "geckodriver.exe");
+                // MarionetteDriverManager.getInstance().setup();
+                // driver = new MarionetteDriver();
+                // System.setProperty("webdriver.gecko.driver",
+                // "src/test/resources");
+                driver = new MarionetteDriver();
+                //driver = new FirefoxDriver();
+                //String marionetteDriverLocation = currentDir + "/tools/marionette/wires.exe";
+                //System.setProperty("webdriver.gecko.driver", marionetteDriverLocation);
+                //driver = new MarionetteDriver();
+                // DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+                // capabilities.setCapability("marionette", true);
+                // driver = new FirefoxDriver(capabilities);
+                break;
+        }
+        return driver;
+    }
 
-		String browser = System.getProperty("Browser");
-		if (browser == null)
-			browser = PropertyManager.getProperty("Browser");
-		System.out.println(browser);
-		for (BrowserType btype : BrowserType.values()) {
-			if (btype.getBrowserName().equalsIgnoreCase(browser)) {
-				type = btype;
-				break;
-			}
+    public static String setDriverUniversity(String university) throws IOException {
 
-		}
+        return "https://" + university + ".tegrity.com";
+    }
 
-		if (type != null) {
-			ATUReports.add("browser was found", LogAs.PASSED, null);
-		} else {
-			ATUReports.add("browser was not found", LogAs.FAILED, null);
-		}
-		return type;
-	}
+    public static BrowserType getBrowserTypeByProperty() {
+        BrowserType type = null;
+
+        String browser = System.getProperty("Browser");
+        if (browser == null)
+            browser = PropertyManager.getProperty("Browser");
+        System.out.println(browser);
+        for (BrowserType btype : BrowserType.values()) {
+            if (btype.getBrowserName().equalsIgnoreCase(browser)) {
+                type = btype;
+                break;
+            }
+
+        }
+
+        if (type != null) {
+            ATUReports.add("browser was found", LogAs.PASSED, null);
+        } else {
+            ATUReports.add("browser was not found", LogAs.FAILED, null);
+        }
+        return type;
+    }
 
 
-	
 }
