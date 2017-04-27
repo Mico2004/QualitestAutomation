7694b3349d2a86e97d47e3d5fcda93db4f437f30
Ilan <ilan.adisa@qualitest.co.il>
2017-04-27 12:21:24 +0300

    Set university name in system variabile

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/LoginHelperPage.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/LoginHelperPage.java
@@ -58,7 +58,7 @@ public class LoginHelperPage extends Page {
 		//setPageUrl("https://awsserverautomation-qa-5.tegrity.com");	
 		//setPageUrl("https://awsserverautomation3-qabr.tegrity.com");
 		//setPageUrl("https://awsserverautomation-perf-5.tegrity.com");	
-		//setPageUrl(DriverSelector.setDriverUniversity(System.getProperty("University")));
+		setPageUrl(DriverSelector.setDriverUniversity(System.getProperty("University")));
 
 	}
 
