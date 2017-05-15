9add90c873b1b95e7620370a7a25e500af401970
Ilan <ilan.adisa@qualitest.co.il>
2017-05-15 14:48:48 +0300

    Waiting 2 min for spinner disappear

--- WebDriverTest/src/test/java/com/automation/main/page_helpers/SearchPage.java
+++ WebDriverTest/src/test/java/com/automation/main/page_helpers/SearchPage.java
@@ -267,7 +267,7 @@ public class SearchPage extends Page {
 	
 	// This function waits until spinner image disappear
 	public void waitUntilSpinnerImageDisappear() throws InterruptedException {
-		for(int i=0; i<60; i++) {
+		for(int i=0; i<120; i++) {
 			try {
 				Thread.sleep(1000);
 				if(loading_spinner.isDisplayed()) {
